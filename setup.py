f325670eb497923dd6be0cdbb69cf838c5014019
yash714 <yashsharma09147@gmail.com>
2023-06-05 01:24:42 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="yash",
+    author_email="yashsharma09147@gmail.com"
+    packages=find_packages(),
+    install_requires=[],
+)
+
